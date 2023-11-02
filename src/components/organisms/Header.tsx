2194e56aae554c4785ca5de774cb9df25127b9a8
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2023-11-02 10:23:03 +0000

    Update Header (semantic HTML)

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -4,11 +4,11 @@ import Nav from "../molecules/Nav";
 
 export default function Header() {
   return (
-    <div className="fixed top-0 z-10 flex w-full items-center justify-between bg-white px-16 py-8">
+    <header className="fixed top-0 z-10 flex w-full items-center justify-between bg-white px-16 py-8">
       <Link href="/">
         <LargeLogo />
       </Link>
       <Nav />
-    </div>
+    </header>
   );
 }
