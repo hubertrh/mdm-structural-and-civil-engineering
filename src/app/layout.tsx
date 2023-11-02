d747a84d4933e571d11fe2b621c8a799e5d7b945
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2023-11-02 09:50:27 +0000

    Change quotes style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,32 @@
-import type { Metadata } from 'next';
-import { Montserrat } from 'next/font/google';
-import localFont from 'next/font/local';
-import Header from '@/src/components/organisms/Header';
-import './globals.css';
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import localFont from "next/font/local";
+import Header from "@/src/components/organisms/Header";
+import "./globals.css";
 
 const montserrat = Montserrat({
-  weight: ['400', '500', '600', '700'],
-  subsets: ['latin'],
-  display: 'swap',
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+  display: "swap",
 });
 
 const agencyFB = localFont({
   src: [
     {
-      path: './fonts/agency-regular.ttf',
-      weight: '400',
+      path: "./fonts/agency-regular.ttf",
+      weight: "400",
     },
     {
-      path: './fonts/agency-bold.ttf',
-      weight: '700',
+      path: "./fonts/agency-bold.ttf",
+      weight: "700",
     },
   ],
-  display: 'swap',
-  variable: '--font-agencyfb',
+  display: "swap",
+  variable: "--font-agencyfb",
 });
 
 export const metadata: Metadata = {
-  title: 'MDM Structural and Civil Engineering',
+  title: "MDM Structural and Civil Engineering",
   description:
     "MDM Engineering: London-based experts in structural design, committed to safety, innovation, and sustainable building solutions. Transforming the city's skyline since 2017.",
 };
