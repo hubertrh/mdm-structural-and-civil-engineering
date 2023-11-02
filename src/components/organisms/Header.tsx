35e2f6d36b513d2f309ac35455d7736ae15c6d22
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2023-11-02 23:38:51 +0000

    Update header stack order

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -4,7 +4,7 @@ import Nav from "../molecules/Nav";
 
 export default function Header() {
   return (
-    <header className="fixed top-0 z-20 flex w-full items-center justify-between bg-white px-16 py-8">
+    <header className="fixed top-0 z-30 flex w-full items-center justify-between bg-white px-16 py-8">
       <Link href="/">
         <LargeLogo />
       </Link>
