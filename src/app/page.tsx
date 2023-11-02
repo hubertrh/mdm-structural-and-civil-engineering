956fed02ef6bdf62e9eaf88fae9b25ae32a38ef0
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2023-11-02 11:18:33 +0000

    Add Welcome component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Hero from "../components/organisms/Hero";
+import Welcome from "../components/organisms/Welcome";
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <Welcome />
+    </>
+  );
 }
