30e3a894f943dfcddbf460561ff6f193eb1e0280
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2023-11-07 01:38:43 +0000

    Make fonts responsive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="text-sm sm:text-base">
       <body
         className={`${montserrat.className} ${agencyFB.variable} mx-auto bg-background-white text-black-300`}
       >
