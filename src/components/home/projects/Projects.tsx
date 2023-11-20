b35e1ac7f68332cd2a22ff730d781e2501d93dd1
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2023-11-20 02:03:45 +0000

    Fix styling on mobile

--- src/components/home/projects/Projects.tsx
+++ src/components/home/projects/Projects.tsx
@@ -50,8 +50,8 @@ export default function Projects() {
                 fill={true}
                 sizes="100dvw"
               />
-              <div className="group absolute top-[102%] grid h-20 w-full place-items-center bg-blue-dark  bg-opacity-0 transition-all duration-300 hover:bg-opacity-40 sm:inset-0 sm:h-auto sm:rounded-none">
-                <div className="home-projects-text grid h-20 w-full place-items-center rounded-lg bg-blue-dark bg-opacity-100 px-4 text-center text-white opacity-100 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100 sm:translate-y-1 sm:rounded-none sm:opacity-0">
+              <div className="group absolute top-[102%] grid h-20 w-full place-items-center bg-blue-dark bg-opacity-0 transition-all duration-300 hover:bg-opacity-40 sm:inset-0 sm:h-auto">
+                <div className="home-projects-text grid h-20 w-full place-items-center bg-blue-dark bg-opacity-100 px-4 text-center text-white opacity-100 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100 sm:translate-y-1 sm:opacity-0">
                   {project.title}
                 </div>
               </div>
--- src/components/home/welcome/Welcome.tsx
+++ src/components/home/welcome/Welcome.tsx
@@ -1,6 +1,6 @@
 export default function Welcome() {
   return (
-    <section className="mx-auto flex w-[80vw] max-w-3xl flex-col gap-5 py-20 text-center text-lg leading-8 sm:py-28">
+    <section className="mx-auto flex w-[85vw] max-w-3xl flex-col gap-5 py-20 text-center text-lg leading-8 sm:py-28">
       <p>
         Welcome to MDM, your specialists in structural engineering for London
         and beyond. We combine fresh perspectives with qualified expertise to
@@ -11,7 +11,7 @@ export default function Welcome() {
         innovation, we ensure the safety, integrity and longevity of your
         structures.
       </p>
-      <h2 className="mt-8 text-2xl">
+      <h2 className="mt-10 text-2xl sm:mt-8">
         We don&apos;t just engineer structures — we craft legacies
       </h2>
     </section>
