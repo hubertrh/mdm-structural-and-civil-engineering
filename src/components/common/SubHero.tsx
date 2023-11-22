58f56e7c1a36062bd59af771bd13964a758c9d94
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2023-11-22 16:29:07 +0000

    Make Subhero responsive

--- src/components/common/SubHero.tsx
+++ src/components/common/SubHero.tsx
@@ -7,8 +7,7 @@ type SubHero = {
 
 export default function SubHero({ variant }: SubHero) {
   return (
-    // <div className="relative top-[8.5rem] grid h-fit w-full place-items-center">
-    <div className="relative mt-[8.5rem] grid h-fit w-full place-items-center">
+    <div className="relative mt-[4.6rem] grid h-fit w-full place-items-center sm:mt-[4.6rem] md:mt-[6.6rem] lg:mt-[8.6rem]">
       <h1 className="absolute inset-0 grid place-items-center text-4xl text-white">
         {variant}
       </h1>
