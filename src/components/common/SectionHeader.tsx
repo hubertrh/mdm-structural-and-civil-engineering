a096efbb56ecd5ea4fab7de1da16cf3c435d9335
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2023-11-23 02:45:10 +0000

    Add variant to SectionHeader component

--- src/components/common/SectionHeader.tsx
+++ src/components/common/SectionHeader.tsx
@@ -1,15 +1,25 @@
 type SectionHeaderProps = {
   title: string;
   description?: string[];
+  variant?: "light";
 };
 
 export default function SectionHeader({
   title,
   description,
+  variant,
 }: SectionHeaderProps) {
   return (
     <section className="flex w-[80vw] max-w-4xl flex-col gap-2 text-justify sm:text-center">
-      <h2 className="pb-4 text-center text-xl font-medium text-black-200 sm:text-3xl">
+      <h2
+        className={`pb-4 text-center text-black-200
+      ${
+        variant === "light"
+          ? "text-lg font-normal sm:text-2xl"
+          : "text-xl font-medium sm:text-3xl"
+      }
+      `}
+      >
         {title}
       </h2>
       {description?.map((paragraph, index) => (
