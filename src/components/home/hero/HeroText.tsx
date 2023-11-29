db68d8ec519c888a84842ecd6dcea79493c58442
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2023-11-29 09:39:27 +0000

    Fix ESLint error

--- src/components/home/hero/HeroText.tsx
+++ src/components/home/hero/HeroText.tsx
@@ -6,10 +6,10 @@ const headingText =
 export default function HeroText() {
   return (
     <div className="hero-text absolute right-0 top-1/4 z-20 mx-14 my-12 text-right text-white">
-      <h3 className="xs:text-5xl mb-8 text-3xl font-medium ">
+      <h3 className="mb-8 text-3xl font-medium xs:text-5xl ">
         Transform your vision into reality
       </h3>
-      <div className="xs:text-2xl text-xl">
+      <div className="text-xl xs:text-2xl">
         <SlideUpH2 headingText={headingText} />
       </div>
     </div>
