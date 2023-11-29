1e96cb393be353a8052b133cf71f1329bf482ebf
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2023-11-29 08:22:26 +0000

    Replace h2 with SlideUpH2 component

--- src/components/home/hero/HeroText.tsx
+++ src/components/home/hero/HeroText.tsx
@@ -1,13 +1,17 @@
+import SlideUpH2 from "@/components/common/SlideUpH2";
+
+const headingText =
+  "Specialising in structural design and assessment, we make safety and innovation our business";
+
 export default function HeroText() {
   return (
     <div className="hero-text absolute right-0 top-1/4 z-20 mx-14 my-12 text-right text-white">
-      <h2 className="mb-8 text-3xl font-medium xs:text-5xl">
+      <h3 className="xs:text-5xl mb-8 text-3xl font-medium ">
         Transform your vision into reality
-      </h2>
-      <h3 className="text-xl xs:text-2xl">
-        Specialising in structural design and assessment,
-        <br /> we make safety and innovation our business
       </h3>
+      <div className="xs:text-2xl text-xl">
+        <SlideUpH2 headingText={headingText} />
+      </div>
     </div>
   );
 }
