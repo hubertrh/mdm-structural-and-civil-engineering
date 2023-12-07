c40b2f7cd7433febf7c236be43ca080c5c4148e9
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2023-12-07 19:27:44 +0000

    Refactor ServicesCards component to improve readability and maintainability
    - Reorganized the JSX structure for better visual hierarchy
    - Updated CSS classes for improved responsiveness and grid layout
    - Simplified logic for previous and next position navigation buttons
    - Refactored code to remove unnecessary ternary operators

--- src/components/services/ServicesCards.tsx
+++ src/components/services/ServicesCards.tsx
@@ -46,67 +46,81 @@ export default function ServicesCards() {
     position !== 1 ? setPosition(position + 1) : null;
 
   return (
-    <section className="relative flex w-[80vw] max-w-6xl items-center justify-center overflow-x-clip md:w-[90vw]">
-      <div
+    <section className="grid place-items-center gap-14">
+      <h2 className="max-w-[80vw] text-center text-2xl sm:text-[1.6rem]">
+        <span>Our&nbsp;Role&nbsp;in&nbsp;Building</span>{" "}
+        <span>Resilient&nbsp;Structures</span>
+      </h2>
+      <div className="relative flex w-[85vw] max-w-6xl items-center justify-center overflow-x-clip md:w-[90vw]">
+        <div
           className="translate-reset flex w-max flex-col gap-20 transition-all duration-500 ease-in-out-quint md:flex-row md:gap-0"
-        style={{ translate: `${position * 33.33}%` }}
-      >
-        {services.map((service, index) => {
-          return (
-            <ServicesCard
-              key={index}
-              title={service.title}
-              summary={service.summary}
-              description={service.description}
-              imgURL={service.imgURL}
-            />
-          );
-        })}
-      </div>
-      <div
-        className="absolute left-0 top-0 hidden h-full w-[8%] place-items-center bg-gradient-to-l from-transparent to-white md:grid lg:w-[15%]"
-        onClick={prevPosition}
-      >
-        {position < 1 ? (
-          <>
-            <div className="absolute left-1/2 top-1/2 h-8 w-8 -translate-x-1/2 -translate-y-1/2 rounded-full bg-white" />
-            <button className="rotate-90 rounded-full">
-              <LottieIcon icon={SliderNavIcon} duration={1000} sizeInRem={3} />
-            </button>
-          </>
-        ) : null}
-      </div>
-      <div
-        className="absolute right-0 top-0 hidden h-full w-[8%] place-items-center bg-gradient-to-r from-transparent to-white md:grid lg:w-[15%]"
-        onClick={nextPosition}
-      >
-        {position > -1 ? (
-          <>
-            <div className="absolute left-1/2 top-1/2 h-8 w-8 -translate-x-1/2 -translate-y-1/2 rounded-full bg-white" />
-            <button className="-rotate-90 rounded-full">
-              <LottieIcon icon={SliderNavIcon} duration={1000} sizeInRem={3} />
-            </button>
-          </>
-        ) : null}
-      </div>
-      <div className="absolute -bottom-4 left-1/2 z-10 hidden -translate-x-1/2 gap-4 md:flex">
-        {services.map((_, index) => {
-          return (
-            <div
-              key={index}
+          style={{ translate: `${position * 33.33}%` }}
+        >
+          {services.map((service, index) => {
+            return (
+              <ServicesCard
+                key={index}
+                title={service.title}
+                summary={service.summary}
+                description={service.description}
+                imgURL={service.imgURL}
+              />
+            );
+          })}
+        </div>
+        <div
+          className="absolute left-0 top-0 hidden h-full w-[8%] place-items-center bg-gradient-to-l from-transparent to-white md:grid lg:w-[18%]"
+          onClick={prevPosition}
+        >
+          {position < 1 ? (
+            <>
+              <div className="absolute left-1/2 top-1/2 h-8 w-8 -translate-x-1/2 -translate-y-1/2 rounded-full bg-white" />
+              <button className="rotate-90 rounded-full">
+                <LottieIcon
+                  icon={SliderNavIcon}
+                  duration={1000}
+                  sizeInRem={3}
+                />
+              </button>
+            </>
+          ) : null}
+        </div>
+        <div
+          className="absolute right-0 top-0 hidden h-full w-[8%] place-items-center bg-gradient-to-r from-transparent to-white md:grid lg:w-[18%]"
+          onClick={nextPosition}
+        >
+          {position > -1 ? (
+            <>
+              <div className="absolute left-1/2 top-1/2 h-8 w-8 -translate-x-1/2 -translate-y-1/2 rounded-full bg-white" />
+              <button className="-rotate-90 rounded-full">
+                <LottieIcon
+                  icon={SliderNavIcon}
+                  duration={1000}
+                  sizeInRem={3}
+                />
+              </button>
+            </>
+          ) : null}
+        </div>
+        <div className="absolute -bottom-4 left-1/2 z-10 hidden -translate-x-1/2 gap-4 md:flex">
+          {services.map((_, index) => {
+            return (
+              <div
+                key={index}
                 className={`grid h-3 w-5 cursor-pointer place-items-center transition-all duration-500 ease-in-out-quint ${
-                position * -1 + 1 === index
-                  ? "outline outline-green"
-                  : "outline outline-transparent"
-              }`}
-              onClick={() => {
-                setPosition((index - 1) * -1);
-              }}
-            >
-              <span className="h-1 w-3 bg-green-dark" />
-            </div>
-          );
-        })}
+                  position * -1 + 1 === index
+                    ? "outline outline-green"
+                    : "outline outline-transparent"
+                }`}
+                onClick={() => {
+                  setPosition((index - 1) * -1);
+                }}
+              >
+                <span className="h-1 w-3 bg-green-dark" />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
