0ffafc61c33e1666e602f542b0c644d806bfa306
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2023-12-08 00:34:56 +0000

    Add a FIXME comment

--- src/components/contexts/LenisContext.tsx
+++ src/components/contexts/LenisContext.tsx
@@ -4,3 +4,5 @@ import { createContext } from "react";
 const LenisContext = createContext<Lenis | null>(null);
 
 export default LenisContext;
+
+// FIXME: Use LenisContext to create custom Link component instead of Next.js Link due to the issue of not being able to scroll to the top of the page when navigating to a new page
