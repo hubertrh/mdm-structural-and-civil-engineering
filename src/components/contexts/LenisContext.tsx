468b833591fb08758ae05739c8cce4671e1bf25e
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2023-12-11 18:31:27 +0000

    Remove Lenis due to accessibility issues
    - Remove "@studio-freight/lenis" dependency from package-lock.json and package.json
    - Remove import statements and references to "@studio-freight/lenis" in layout.tsx, Mapbox.tsx, LenisContext.tsx, and SmoothScrollContext.tsx
    - Delete LenisContext.tsx and SmoothScrollContext.tsx files

--- src/components/contexts/LenisContext.tsx
+++
@@ -1,8 +0,0 @@
-import Lenis from "@studio-freight/lenis";
-import { createContext } from "react";
-
-const LenisContext = createContext<Lenis | null>(null);
-
-export default LenisContext;
-
-// FIXME: Use LenisContext to create custom Link component instead of Next.js Link due to the issue of not being able to scroll to the top of the page when navigating to a new page
