bc40d596953becff6abc0869a0f9adfb23485792
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2023-12-14 10:23:20 +0000

    Add groqGetters.ts for fetching contact page, company details, services page info, and services cards
    This commit adds a new file `groqGetters.ts` which contains functions for fetching data from Sanity CMS. The functions `getContactPage`, `getCompanyDetails`, `getServicesPageInfo`, and `getServicesCards` use the `createClient` and `groq` methods from the "next-sanity" package to construct queries and fetch data from the Sanity API.
    
    The `getContactPage` function fetches the header and paragraphs of the contact page. The `getCompanyDetails` function fetches various details about the company such as name, email, phone number, address, and Google Maps link. The `getServicesPageInfo` function fetches the header and paragraphs of the services page. Lastly, the `getServicesCards` function fetches information about individual service cards including title, short description, paragraphs, and image URL with metadata.
    
    These functions will be used to retrieve data from Sanity CMS in other parts of the codebase.

---
+++ src/sanity/utils/groqGetters.ts
@@ -0,0 +1,75 @@
+import { createClient, groq } from "next-sanity";
+import clientConfig from "./clientConfig";
+import { ContactPage } from "@/types/sanityGetters/contactPage.type";
+import {
+  ServicesCards,
+  ServicesPageInfo,
+} from "@/types/sanityGetters/servicesPage.type";
+import { companyDetails } from "@/types/sanityGetters/companyDetails.type";
+
+const sanityClient = createClient(clientConfig);
+
+export async function getContactPage(): Promise<ContactPage> {
+  const query = groq`
+    *[_type == "contactPage"][0] {
+      header,
+      paragraphs[] {
+        text
+      }
+    }
+  `;
+
+  return sanityClient.fetch(query);
+}
+
+export async function getCompanyDetails(): Promise<companyDetails> {
+  const query = groq`
+    *[_type == "companyInfo"][0] {
+      name,
+      nameNarrow {
+        firstLine,
+        secondLine,
+      },
+      email,
+      phone,
+      address,
+      addressNarrow {
+        firstLine,
+        secondLine,
+      },
+      googleMapsLink
+    }
+  `;
+
+  return sanityClient.fetch(query);
+}
+
+export async function getServicesPageInfo(): Promise<ServicesPageInfo> {
+  const query = groq`
+    *[_type == "servicesPage"][0] {
+      header,
+      paragraphs[] {
+        text
+      }
+    }
+  `;
+
+  return sanityClient.fetch(query);
+}
+
+export async function getServicesCards(): Promise<ServicesCards> {
+  const query = groq`
+  *[_type == "servicesPage"][0].services[] {
+    title,
+    shortDescription,
+    paragraphs,
+    "image": image.asset->{
+      url,
+      metadata {
+        lqip
+      }
+    }
+  }`;
+
+  return sanityClient.fetch(query);
+}
