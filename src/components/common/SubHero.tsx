0136485df01d4be19b47e7f601828166d26f5aec
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2024-01-10 10:58:34 +0000

    Refactor SubHero component to remove unnecessary CSS class in JSX
    The SubHero component has been refactored to remove the "sm:mt-[4.6rem]" CSS class from the inner div, as it was redundant and not being used. This change simplifies the code and improves readability.

--- src/components/common/SubHero.tsx
+++ src/components/common/SubHero.tsx
@@ -8,7 +8,7 @@ type SubHero = {
 export default function SubHero({ variant }: SubHero) {
   return (
     <div className="relative flex w-full items-center justify-center overflow-hidden">
-      <div className="subhero relative mt-[4.6rem] grid h-fit w-full min-w-[768px] place-items-center sm:mt-[4.6rem] md:mt-[6.6rem] lg:mt-[8.6rem]">
+      <div className="subhero relative mt-[4.6rem] grid h-fit w-full min-w-[768px] place-items-center md:mt-[6.6rem] lg:mt-[8.6rem]">
         <h1 className="absolute inset-0 grid place-items-center text-2xl text-white sm:text-4xl">
           {variant}
         </h1>
