93a0db4cf76b27287c24fc6d2e832ca6287fe670
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2024-02-03 20:57:05 +0000

    Refactor project pagination to use custom hook
    The code has been refactored to move the logic for project pagination into a new custom hook, `useProjectsPagination`. This simplifies the `ProjectsGrid` component by abstracting away the state management and data fetching related to project pagination. The IntersectionObserver setup for infinite scrolling has also been simplified in the process.

--- src/components/projects/ProjectsGrid.tsx
+++ src/components/projects/ProjectsGrid.tsx
@@ -2,94 +2,34 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { useState, useEffect, useRef, useCallback } from "react";
+import { useEffect, useRef } from "react";
 import ProjectsGridSkeleton from "./ProjectsGridSkeleton";
-import {
-  getInitialProjects,
-  getMoreProjects,
-} from "@/sanity/groqGetters/pages/projects";
-import { Project } from "@/types/groqGetters/project.type";
-import formatDateToISOString from "@/utils/formatDateToISOString";
+import { useProjectsPagination } from "@/hooks/useProjectsPagination";
 
 export default function ProjectsGrid() {
-  const [projects, setProjects] = useState<Project[]>([]);
-  const lastProjectDetailsRef = useRef({
-    lastCompletionDate: new Date(),
-    lastName: "",
-    lastId: "",
-  });
-  const [isLoading, setIsLoading] = useState(true);
-  const [isMoreLoading, setIsMoreLoading] = useState(false);
-  const [hasMoreProjects, setHasMoreProjects] = useState(false);
-  const observer = useRef<IntersectionObserver | null>(null);
+  const { projects, isLoading, isMoreLoading, loadMoreProjects } =
+    useProjectsPagination();
+
+  // Ref for the sentinel element used in infinite scrolling
   const sentinelRef = useRef<HTMLDivElement | null>(null);
 
-  // Load initial projects
+  // Initialize IntersectionObserver for infinite scrolling
   useEffect(() => {
-    getInitialProjects().then((initialProjects) => {
-      setProjects(initialProjects);
-      setIsLoading(false);
+    if (!loadMoreProjects) return;
 
-      if (initialProjects.length > 0) {
-        const lastProject = initialProjects[initialProjects.length - 1];
-        lastProjectDetailsRef.current = {
-          lastCompletionDate: new Date(lastProject.completionDate),
-          lastName: lastProject.name,
-          lastId: lastProject._id,
-        };
-        setHasMoreProjects(initialProjects.length >= 10);
-      }
-    });
-  }, []);
-
-  const loadMoreProjects = useCallback(async () => {
-    if (isMoreLoading || !hasMoreProjects) return;
-
-    setIsMoreLoading(true);
-
-    const { lastCompletionDate, lastName, lastId } =
-      lastProjectDetailsRef.current;
-    const dateObj = new Date(lastCompletionDate);
-    const formattedDate = formatDateToISOString(dateObj);
-    const newProjects = await getMoreProjects(formattedDate, lastName, lastId);
-
-    if (newProjects.length > 0) {
-      setProjects((prev) => [...prev, ...newProjects]);
-
-      const lastNewProject = newProjects[newProjects.length - 1];
-
-      lastProjectDetailsRef.current = {
-        lastCompletionDate: new Date(lastNewProject.completionDate),
-        lastName: lastNewProject.name,
-        lastId: lastNewProject._id,
-      };
-      setHasMoreProjects(newProjects.length >= 5);
-    } else {
-      setHasMoreProjects(false);
-    }
-    setIsMoreLoading(false);
-  }, [isMoreLoading, hasMoreProjects]);
-
-  // Observer effect for infinite scrolling
-  useEffect(() => {
-    const observerCallback = (entries: IntersectionObserverEntry[]) => {
-      if (entries[0].isIntersecting) {
-        loadMoreProjects();
-      }
-    };
-    const observerOptions = {
-      root: null,
-      rootMargin: "0px 0px 1000px 0px",
-      threshold: 0.01,
-    };
-    observer.current = new IntersectionObserver(
-      observerCallback,
-      observerOptions,
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) loadMoreProjects();
+      },
+      { root: null, rootMargin: "0px 0px 1000px 0px", threshold: 0.01 },
     );
-    if (sentinelRef.current) observer.current.observe(sentinelRef.current);
-    return () => observer.current?.disconnect();
+
+    if (sentinelRef.current) observer.observe(sentinelRef.current);
+
+    return () => observer.disconnect();
   }, [loadMoreProjects]);
 
+  // Render ProjectsGrid or Skeleton based on loading state
   return (
     <div className="mx-auto w-[85vw] max-w-4xl py-24">
       {isLoading && <ProjectsGridSkeleton />}
