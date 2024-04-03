8416b1fe8b765887b5b5bf13f33d586a1811ecb2
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2024-04-03 11:44:28 +0100

    Add subhero text styling and ESLint rule
    Introduced a new CSS class 'subhero-text' to apply specific styling to the SubHero component. This includes adding a white text shadow for better contrast. Also, updated the .eslintrc.json file to include 'subhero-text' in the list of allowed classes.

--- src/components/common/SubHero.tsx
+++ src/components/common/SubHero.tsx
@@ -15,7 +15,7 @@ export default function SubHero({ variant }: SubHero) {
   return (
     <div className="relative flex w-full items-center justify-center overflow-hidden">
       <div className="relative mt-[4.6rem] grid h-fit w-full min-w-[768px] place-items-center md:mt-[6.6rem] lg:mt-[8.6rem]">
-        <h1 className="absolute inset-0 z-10 grid place-items-center text-3xl font-normal text-blue-dark sm:text-[2.5rem]">
+        <h1 className="subhero-text absolute inset-0 z-10 grid place-items-center text-3xl font-normal text-blue-dark sm:text-[2.5rem]">
           {variant}
         </h1>
         <Image
