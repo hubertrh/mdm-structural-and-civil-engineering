455765ad83065542132a9eef8e1d81939006a3d7
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2024-04-04 18:41:07 +0100

    Add nextjs-toploader to improve UX
    Integrated the 'nextjs-toploader' package into the project. This addition will enhance user experience by providing a loading bar at the top of the page, giving users visual feedback when pages are loading. The loader is customized with specific color, height, initial position, easing function, and speed settings. Also set to hide spinner for cleaner look.

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -4,6 +4,7 @@ import localFont from "next/font/local";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import Script from "next/script";
+import NextTopLoader from "nextjs-toploader";
 import Footer from "@/components/common/footer/Footer";
 import Header from "@/components/common/header/Header";
 import "./globals.css";
@@ -50,6 +51,14 @@ export default function RootLayout({
       <body
         className={`${montserrat.className} ${agencyFB.variable} mx-auto bg-background-white text-black-300`}
       >
+        <NextTopLoader
+          color="#70916C"
+          height={5}
+          initialPosition={0.2}
+          easing="ease"
+          showSpinner={false}
+          speed={500}
+        />
         <div className="min-h-dvh w-full">
           <Header />
           <main className="mx-auto min-h-dvh w-full max-w-screen-2xl bg-white">
