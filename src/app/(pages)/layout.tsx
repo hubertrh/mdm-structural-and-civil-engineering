38d5f9d9051a39053842e7aaeb0339d160824973
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2024-04-05 11:45:05 +0100

    Update metadata in layout
    Added a new `metadataBase` field to the metadata object in the layout file. This field uses environment variables for dynamic URL generation. Also introduced an `alternates` field with a canonical link reference.

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -37,6 +37,12 @@ export const metadata: Metadata = {
   },
   description:
     "MDM Engineering: London experts in structural design, committed to safety, innovation, and sustainable building. Transforming the city's skyline since 2017.",
+  metadataBase: new URL(
+    `https://${process.env.VERCEL_URL || "localhost:3000"}`,
+  ),
+  alternates: {
+    canonical: "/",
+  },
 };
 
 export default function RootLayout({
